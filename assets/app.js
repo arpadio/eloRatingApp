e9609313d26d0b43d5cb0ada9ad0a8f61a843993
Sam Mead <samjamead@gmail.com>
2022-05-23 11:49:57 +0100

    Convert to ES6 (WIP 2 of 2)

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,6 @@
 // App JS
 document.addEventListener("DOMContentLoaded", () => {
 
-  // First some display stuff to change the views
-  $("#newResultButton").click(function() {
-    $("#rankingView").hide("slow");
-    $("#newResultView").show("slow");
-  });
-
-  $(".newPlayerButton").click(function() {
-    $("#rankingView").hide("slow");
-    $("#newResultView").hide("slow");
-    $("#newPlayerView").show("slow");
-  });
-
-  $(".cancel-btn").click(function() {
-    $("#newResultView").hide("slow");
-    $("#newPlayerView").hide("slow");
-    $("#rankingView").show("slow");
-  });
-
-  // But what day is it?!
   const date = new Date();
   const today = date.toDateString();
 
@@ -39,16 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Commonly handled elements
   const rankingTable = document.querySelector('#rankingView table tbody');
-  const gamesCount = document.querySelector('#totalGames');
   const resultsList = document.querySelector('#resultsList');
+  const playerOptions = document.querySelectorAll('.playerOption');
 
   // When the Firebase DB changes...
   dbRef.on('value', function(snapshot) {
 
     const data = snapshot.val();
 
+    // Clear all data to allow live updates
+    rankingTable.innerHTML = '';
+    resultsList.innerHTML = '';
+    playerOptions.forEach((option, i) => {
+      option.remove();
+    });
+
+
     // Push players to array to allow sorting
-    let   rankedPlayers   = [];
+    let rankedPlayers = [];
     for (let player in data.players) {
      rankedPlayers.push({
        id: player,
@@ -89,6 +78,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const updateResultsList = () => {
 
+      let gamesCount = document.getElementById('totalGames');
       gamesCount.innerHTML = `${data.results.length} games played`;
 
       const results = data.results.reverse();
@@ -128,12 +118,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
     updateResultSelects();
 
-    $("#submitResult").click(function() {
+    // Submitting new results
 
-      // Find out who beat who
-      let p1 = document.querySelector("#playerOne option:checked").textContent;
-      let p2 = document.querySelector("#playerTwo option:checked").textContent;
+    const sumbitResult = document.getElementById('submitResult');
+    submitResult.addEventListener('click', (event) => {
 
+      const winner = document.querySelector("#playerOne option:checked");
+      const loser = document.querySelector("#playerTwo option:checked");
+      let p1 = winner.textContent;
+      let p2 = loser.textContent;
+
+      // Basic validation
+      if (p1 === '' && p2 === '') {
+        winnerError.classList.add('form-error-active');
+        loserError.innerHTML = 'Please select the loser';
+        loserError.classList.add('form-error-active');
+        return false
+      } else if (p1 === '')  {
+        winnerError.classList.add('form-error-active');
+        return false
+      }
+      if (p2 === '') {
+        loserError.innerHTML = 'Please select the loser'
+        loserError.classList.add('form-error-active');
+        return false
+      }
+      if (p1 === p2) {
+        loserError.innerHTML = 'The players must be different';
+        loserError.classList.add('form-error-active');
+        return false
+      }
+
+      // If validation passes...
       // Initialise all variables used in ranking algorithm
       let p1ID;
       let p2ID;
@@ -175,9 +191,10 @@ document.addEventListener("DOMContentLoaded", () => {
       let s1 = 1;
       let s2 = 0;
 
-      // Introduce 'the constant', aka the K factor
-      // We'll use 10 because the Fédération Internationale des Échecs does for anyone over 2400. Pro!
-      let k = 10
+      // The K factor
+      // Fédération Internationale des Échecs uses 10 for anyone over 2400.
+      // Elo originally recommended 16 for grandmasters and 32 for amateurs
+      let k = 16
 
       // Calculate new rankings
       let p1n = p1r + k * (s1 - e1);
@@ -216,29 +233,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Prevents undiagnosed empty scores
       location.reload(true);
-
     });
 
-  });
+  }); // End on 'value'
 
-  $("#addPlayer").click(function() {
+  // Adding new players
+  const addPlayer = document.getElementById('addPlayer');
+  addPlayer.addEventListener('click', (event) => {
 
-    $(".helpText").remove();
-
-    const npFirst = $.trim($('#npFirst').val())
-    const npLast = $.trim($('#npLast').val())
-
+    let npFirst = document.getElementById('npFirst').value;
+    npFirst = npFirst.trim();
+    let npLast = document.getElementById('npLast').value;
+    npLast = npLast.trim();
     let npName = `${npFirst} ${npLast}`;
-    let npNationality = $.trim($('#npNationality').val());
+
+    let npNationality = document.getElementById('npNationality').value;
+    npNationality = npNationality.trim();
 
     // Some basic form validation
-    if ((npName == '') && (npNationality == '')) {
-      $('#npName').before("<p class=\"helpText\">Please enter the new player's <strong>name<strong></p>");
-      $('#npNationality').before("<p class=\"helpText\">Please enter the new player's <strong>nationality<strong></p>");
-    } else if (npName == '') {
-      $('#npName').before("<p class=\"helpText\">Please enter the new player's <strong>name<strong></p>");
-    } else if (npNationality == '') {
-      $('#npNationality').before("<p class=\"helpText\">Please enter the new player's <strong>nationality<strong></p>");
+    if (npFirst == '') {
+      document.getElementById('firstError').classList.add('form-error-active');
+    }
+    if (npLast == '') {
+      document.getElementById('lastError').classList.add('form-error-active');
+    }
+    if (npNationality == '') {
+      document.getElementById('nationalityError').classList.add('form-error-active');
+    }
+    if (npFirst == '' || npLast == '' || npNationality == '') {
+      return false;
     } else {
       firebase.database().ref('/players/').push({
         name: npName,
@@ -254,12 +277,88 @@ document.addEventListener("DOMContentLoaded", () => {
 
   });
 
-  $("#resetButton").click(function() {
-    $.getJSON('./data/reset.json', function (resetData) {
-      console.log(resetData);
+  // Control the views
+  const rankingView = document.getElementById('rankingView');
+  const newResultView = document.getElementById('newResultView');
+  const newPlayerView = document.getElementById('newPlayerView');
+
+  const newResultButton = document.getElementById('newResultButton');
+  const newPlayerButton = document.querySelectorAll('.newPlayerButton');
+  const cancelButton = document.querySelectorAll('.cancel-btn');
+
+  newResultButton.addEventListener('click', () => {
+    rankingView.style.display = "none";
+    newPlayerView.style.display = "none";
+    newResultView.style.display = "block";
+  })
+
+  newPlayerButton.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      rankingView.style.display = "none";
+      newResultView.style.display = "none";
+      newPlayerView.style.display = "block";
+    })
+  });
+
+  cancelButton.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      newResultView.style.display = "none";
+      newPlayerView.style.display = "none";
+      rankingView.style.display = "block";
+    })
+  });
+
+  // Remove form validation when errors are fixed
+  const npFirstField = document.querySelector('#npFirst');
+  const npLastField = document.querySelector('#npLast');
+  const npNationality = document.querySelector('#npNationality');
+  const winnerSelect = document.querySelector('#playerOne');
+  const loserSelect = document.querySelector('#playerTwo');
+
+  const removeFieldErrors = (errorID) => {
+    if (event.target.value != '') {
+      document.getElementById(errorID).classList.remove('form-error-active');
+    }
+  }
+
+  npFirstField.addEventListener('blur', (event) => {
+    removeFieldErrors('firstError');
+  });
+
+  npLastField.addEventListener('blur', (event) => {
+    removeFieldErrors('lastError');
+  });
+
+  npNationality.addEventListener('blur', (event) => {
+    removeFieldErrors('nationalityError');
+  });
+
+  const removeSelectErrors = (errorID) => {
+    if (event.target.value != 'none') {
+      document.getElementById(errorID).classList.remove('form-error-active');
+    }
+  }
+
+  winnerSelect.addEventListener('change', (event) => {
+    removeSelectErrors('winnerError');
+  });
+
+  loserSelect.addEventListener('change', (event) => {
+    removeSelectErrors('winnerError');
+  });
+
+
+  // Reset all data if required
+  const resetButton = document.getElementById('resetButton');
+  resetButton.addEventListener('click', (event) => {
+    fetch('./assets/reset.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(resetData => {
       firebase.database().ref().set(resetData);
       location.reload(true);
-    });
+    })
   });
 
 });
